decda0157e429b1efd44169a9f556068b82d2c89
Marius Lundgård <studio@mariuslundgard.com>
2023-03-08 12:01:32 +0100

    chore(test-studio): add document inspector example

--- dev/test-studio/schema/index.ts
+++ dev/test-studio/schema/index.ts
@@ -49,6 +49,7 @@ import focus from './debug/focus'
 import gallery from './debug/gallery'
 import {hoistedPt, hoistedPtDocument, customBlock} from './debug/hoistedPt'
 import {initialValuesTest, superlatives} from './debug/initialValuesTest'
+import {inspectorsTestType} from './debug/inspectors'
 import invalidPreviews from './debug/invalidPreviews'
 import {languageFilterDebugType} from './debug/languageFilter'
 import liveEdit from './debug/liveEdit'
@@ -228,6 +229,7 @@ export const schemaTypes = [
   spotifyEmbed,
   strings,
   superlatives,
+  inspectorsTestType,
   texts,
   topLevelArrayType,
   topLevelPrimitiveArrayType,
