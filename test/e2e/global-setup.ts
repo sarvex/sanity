599c466143a514abd5b987028fd38d74828bc386
Marius Lundgård <studio@mariuslundgard.com>
2023-04-14 14:31:06 +0200

    test: let playwright fail faster when missing env variables

--- test/e2e/global-setup.ts
+++ test/e2e/global-setup.ts
@@ -1,9 +1,6 @@
-/* eslint-disable no-process-env */
 import path from 'path'
 import {chromium, FullConfig} from '@playwright/test'
-import {loadEnvFiles} from '../../scripts/utils/loadEnvFiles'
-
-loadEnvFiles()
+import {SANITY_E2E_SESSION_TOKEN} from './env'
 
 /**
  * This setup will reuse the authentication state in new browser contexts so that you
@@ -12,14 +9,10 @@ loadEnvFiles()
 async function globalSetup(config: FullConfig): Promise<void> {
   const {baseURL} = config.projects[0].use
   const browser = await chromium.launch()
+
   // Create context to store our session token cookie in
   const context = await browser.newContext()
   const page = await context.newPage()
-  const token = process.env.SANITY_E2E_SESSION_TOKEN
-
-  if (!token) {
-    throw new Error('Missing sanity token - see README.md for details')
-  }
 
   const [response] = await Promise.all([
     // CI is slow to start, make sure we wait long enough
@@ -35,7 +28,7 @@ async function globalSetup(config: FullConfig): Promise<void> {
   await context.addCookies([
     {
       name: 'sanitySession',
-      value: token,
+      value: SANITY_E2E_SESSION_TOKEN,
       sameSite: 'None',
       secure: true,
       httpOnly: true,
@@ -43,6 +36,7 @@ async function globalSetup(config: FullConfig): Promise<void> {
       path: '/',
     },
   ])
+
   // Store the context in a file that will be reused across our tests
   await context.storageState({path: path.join(__dirname, 'state', 'storageState.json')})
   await browser.close()
